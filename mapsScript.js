03aab8405c1c2b6226bbc7e6bcfd29d6cc301651
Albert Wong <a.wong9970@gmail.com>
2017-11-21 18:37:19 -0800

    Added geocoding API calls

--- mapsScript.js
+++ mapsScript.js
@@ -1,22 +1,38 @@
 $(function() {
 	//37.849523, -122.148005
 
-	$("button#ajax").on("click", function() {
+	var myMap = function(latitude, longitude) {
 		var mapProp= {
-		    center:new google.maps.LatLng($('#latitude').val(), $('#longitude').val()),
+		    center:new google.maps.LatLng(latitude, longitude),
 		    zoom:15
 		};
 		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 		var marker = new google.maps.Marker({position:mapProp.center});
 		marker.setMap(map);
-	})
+	}
+	
+	$("#coordinateSubmit").on("click", function() {
+		myMap($('#latitude').val(), $('#longitude').val());
+	});
+
+
 
 	$('#latitude').keypress(function(e){
 	    if(e.keyCode==13)
-	    	$('button#ajax').click();
+	    	$('#coordinateSubmit').click();
 	  });
 	$('#longitude').keypress(function(e){
 	    if(e.keyCode==13)
-	    	$('button#ajax').click();
+	    	$('#coordinateSubmit').click();
 	  });
+	$('#address').keypress(function(e){
+	    if(e.keyCode==13)
+	    	$('#addressSubmit').click();
+	  });
+
+
+
+	$.get("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyDJ94m0-jjU5qbqsdTzAKMlzIBeDgwBJR8", function(response) {
+		  myMap(response.results[0].geometry.location.lat, response.results[0].geometry.location.lng);
+		})
 });
